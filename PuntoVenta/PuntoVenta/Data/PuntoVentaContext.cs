e65a98416c9b0757fef3a157f7134c2785495e14
Jeremy Miller <125916246+JeremyMiller177@users.noreply.github.com>
2023-06-05 18:37:32 -0600

    Se agregó la conexión a la base de datos y la pantalla de Usuarios

---
+++ PuntoVenta/PuntoVenta/Data/PuntoVentaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PuntoVenta.Models;
+
+namespace PuntoVenta.Data
+{
+    public class PuntoVentaContext : DbContext
+    {
+        public PuntoVentaContext (DbContextOptions<PuntoVentaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PuntoVenta.Models.Usuario> Usuario { get; set; } = default!;
+    }
+}
---
+++ PuntoVenta/PuntoVenta/Models/Usuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PuntoVenta.Models
+{
+    public class Usuario
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+            
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        public string Correo { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Contrasena { get; set; }
+
+        #pragma warning disable CS8618
+        public Usuario() { }
+
+        public Usuario(string nombre, string correo, string contrasena)
+        {
+            Nombre = nombre;
+            Correo = correo;
+            Contrasena = contrasena;
+        }
+    }
+}
